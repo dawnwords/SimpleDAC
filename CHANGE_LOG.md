4307b2ce4d1702cc02a2236fa3158a5c7b894bfb
Gu Jingxiao <gujingxiao@gmail.com>
2015-06-25 14:24:30 +0800

    + read & write lock for FileBasedDataAccessComponent + memory based Transaction

--- CHANGE_LOG.md
+++ CHANGE_LOG.md
@@ -4,3 +4,9 @@ v 1.0
 -----------------
 1. JSON解析依赖包由Gson改为FastJson(rocketmq-client依赖已包含)
 2. Condition.True类改为常量Condition.True
+
+
+v 2.0
+-----------------
+1. DACFactory.createDAC()增加线程同步
+2. DACFactory.createDAC()对于同一个BeanClass，返回同一个DAC实例
--- src/main/java/cn/edu/fudan/se/dac/FileBasedDataAccessComponent.java
+++ src/main/java/cn/edu/fudan/se/dac/FileBasedDataAccessComponent.java
@@ -6,6 +6,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by Dawnwords on 2015/5/21.
@@ -13,113 +16,273 @@ import java.util.List;
 final class FileBasedDataAccessComponent<Bean> implements DataAccessInterface<Bean> {
 
     private static final String TEMP_EXT = ".tmp";
-    private static final String OLD_EXT = ".old";
-    private static final String CHANGE_TEMP_EXT = ".ctmp";
 
-    private File dataFile, oldFile, tempFile, changeTempFile;
-    private boolean transaction;
+    private File dataFile, changeTempFile;
+    private List<Action> transaction;
     private Class<Bean> beanClass;
+    private Lock readLock;
+    private Lock writeLock;
+    private ReentrantLock transactionLock;
+    private boolean committing;
+
+    private abstract class Action {
+        private boolean createTemp;
+
+        protected Action(boolean createTemp) {
+            this.createTemp = createTemp;
+        }
+
+        boolean execute() {
+            if (committing) {
+                return modifyLogic();
+            }
+
+            if (transactionLock.isLocked()) {
+                transaction.add(this);
+                return true;
+            }
+
+            writeLock.lock();
+            boolean result = modifyLogic();
+            if (createTemp) {
+                result = result && FileUtil.overwrite(changeTempFile, dataFile);
+            }
+            writeLock.unlock();
+            return result;
+        }
+
+        abstract Bean modifyBean(Bean bean);
+
+        abstract void performTransaction();
+
+        abstract boolean modifyLogic();
+    }
+
+    private class AddAction extends Action {
+        Bean element;
+
+        public AddAction(Bean element) {
+            super(false);
+            this.element = element;
+        }
+
+        @Override
+        Bean modifyBean(Bean bean) {
+            return bean;
+        }
+
+        @Override
+        public void performTransaction() {
+            add(element);
+        }
+
+        @Override
+        boolean modifyLogic() {
+            return FileUtil.appendLine(dataFile, JSON.toJSONString(element));
+        }
+
+    }
+
+    private class DeleteAction extends Action {
+        Condition<Bean> condition;
+
+        public DeleteAction(Condition<Bean> condition) {
+            super(true);
+            this.condition = condition;
+        }
+
+        @Override
+        Bean modifyBean(Bean bean) {
+            return condition.assertBean(bean) ? null : bean;
+        }
+
+        @Override
+        public void performTransaction() {
+            deleteByCondition(condition);
+        }
+
+        @Override
+        boolean modifyLogic() {
+            return FileUtil.getBufferedWriter(changeTempFile, new FileUtil.BufferedWriterHandler() {
+                @Override
+                public boolean handle(BufferedWriter writer) {
+                    return FileUtil.eachLine(dataFile, new FileUtil.OutputLineHandler(writer), new ConditionFilter(condition, beanClass));
+                }
+            });
+        }
+    }
+
+    private class UpdateAction extends Action {
+        Condition<Bean> condition;
+        BeanSetter<Bean> setter;
+
+        public UpdateAction(Condition<Bean> condition, BeanSetter<Bean> setter) {
+            super(true);
+            this.condition = condition;
+            this.setter = setter;
+        }
+
+        @Override
+        Bean modifyBean(Bean bean) {
+            if (condition.assertBean(bean)) setter.set(bean);
+            return bean;
+        }
+
+        @Override
+        public void performTransaction() {
+            updateByCondition(condition, setter);
+        }
+
+        @Override
+        boolean modifyLogic() {
+            return FileUtil.getBufferedWriter(changeTempFile, new FileUtil.BufferedWriterHandler() {
+                @Override
+                public boolean handle(final BufferedWriter writer) {
+                    return FileUtil.eachLine(dataFile, new FileUtil.OutputLineHandler(writer) {
+                        @Override
+                        public void filter(String line) throws Exception {
+                            Bean bean = JSON.parseObject(line, beanClass);
+                            setter.set(bean);
+                            keep(JSON.toJSONString(bean));
+                        }
+                    }, new ConditionFilter(condition, beanClass));
+                }
+            });
+        }
+    }
 
     FileBasedDataAccessComponent(Class<Bean> beanClass) {
         this.beanClass = beanClass;
+        this.transactionLock = new ReentrantLock();
+        this.transaction = new ArrayList<Action>();
+
+        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        this.readLock = readWriteLock.readLock();
+        this.writeLock = readWriteLock.writeLock();
 
         String dataFileName = beanClass.getSimpleName();
         dataFile = new File(dataFileName);
-        oldFile = new File(dataFileName + OLD_EXT);
-        tempFile = new File(dataFileName + TEMP_EXT);
-        changeTempFile = new File(dataFileName + CHANGE_TEMP_EXT);
+        changeTempFile = new File(dataFileName + TEMP_EXT);
 
-        boolean dataExists = dataFile.exists();
-        boolean oldExists = oldFile.exists();
-        boolean tempExists = tempFile.exists();
 
-        if (tempExists) {
-            if (dataExists) {
-                tempFile.delete();
-            } else if (oldExists) {
-                tempFile.renameTo(dataFile);
-            }
-        } else if (dataExists) {
-            if (oldExists) {
-                oldFile.delete();
-            }
-        } else if (changeTempFile.exists()) {
-            changeTempFile.renameTo(dataFile);
+        if (dataFile.exists()) {
+            changeTempFile.deleteOnExit();
         } else {
-            dataFile = FileUtil.createFileIfNotExist(dataFileName);
+            if (changeTempFile.exists()) {
+                FileUtil.overwrite(changeTempFile, dataFile);
+            } else {
+                dataFile = FileUtil.createFileIfNotExist(dataFileName);
+            }
         }
-
-        changeTempFile.deleteOnExit();
     }
 
     @Override
     public boolean beginTransaction() {
-        try {
-            transaction = FileUtil.copy(dataFile, tempFile);
-            return transaction;
-        } catch (Exception e) {
-            return false;
-        }
+        transactionLock.lock();
+        transaction.clear();
+        return true;
     }
 
     @Override
     public boolean commit() {
-        transaction = false;
-        return dataFile.renameTo(oldFile) && tempFile.renameTo(dataFile) && oldFile.delete();
+        if (!transactionLock.isLocked()) {
+            throw new IllegalStateException("no transaction not begins");
+        }
+        committing = true;
+        writeLock.lock();
+        for (Action action : transaction) {
+            action.performTransaction();
+        }
+        writeLock.unlock();
+        transactionLock.unlock();
+        committing = false;
+        return true;
     }
 
     @Override
     public boolean rollback() {
-        transaction = false;
-        return tempFile.delete();
+        if (!transactionLock.isLocked()) {
+            throw new IllegalStateException("no transaction not begins");
+        }
+        transaction.clear();
+        return true;
     }
 
     @Override
     public boolean add(Bean bean) {
-        String json = JSON.toJSONString(bean);
-        return FileUtil.appendLine(getDataFile(), json);
+        if (bean == null) {
+            throw new NullPointerException("bean is null");
+        }
+        return new AddAction(bean).execute();
     }
 
     @Override
     public boolean deleteByCondition(final Condition<Bean> condition) {
-        return FileUtil.getBufferedWriter(changeTempFile, new FileUtil.BufferedWriterHandler() {
-            @Override
-            public boolean handle(BufferedWriter writer) {
-                return FileUtil.eachLine(getDataFile(), new FileUtil.OutputLineHandler(writer), new ConditionFilter(condition, beanClass));
-            }
-        }) && FileUtil.overwrite(changeTempFile, getDataFile());
+        if (condition == null) {
+            throw new NullPointerException("condition is null");
+        }
+        return new DeleteAction(condition).execute();
     }
 
     @Override
     public boolean updateByCondition(final Condition<Bean> condition, final BeanSetter<Bean> setter) {
-        return setter == null || (FileUtil.getBufferedWriter(changeTempFile, new FileUtil.BufferedWriterHandler() {
-            @Override
-            public boolean handle(final BufferedWriter writer) {
-                return FileUtil.eachLine(getDataFile(), new FileUtil.OutputLineHandler(writer) {
-                    @Override
-                    public void filter(String line) throws Exception {
-                        Bean bean = JSON.parseObject(line, beanClass);
-                        setter.set(bean);
-                        keep(JSON.toJSONString(bean));
-                    }
-                }, new ConditionFilter(condition, beanClass));
-            }
-        }) && FileUtil.overwrite(changeTempFile, getDataFile()));
+        if (condition == null) {
+            throw new NullPointerException("condition is null");
+        }
+        if (setter == null) {
+            throw new NullPointerException("setter is null");
+        }
+        return new UpdateAction(condition, setter).execute();
     }
 
     @Override
     public List<Bean> selectByCondition(final Condition<Bean> condition) {
+        if (condition == null) {
+            throw new NullPointerException("condition is null");
+        }
+        readLock.lock();
         final List<Bean> result = new ArrayList<Bean>();
-        FileUtil.eachLine(getDataFile(), new FileUtil.LineHandler() {
+
+        FileUtil.eachLine(dataFile, new FileUtil.LineHandler() {
             @Override
             public void filter(String line) throws Exception {
-                result.add(JSON.parseObject(line, beanClass));
+                Bean bean = JSON.parseObject(line, beanClass);
+                if (transactionLock.isLocked()) {
+                    for (Action action : transaction) {
+                        bean = action.modifyBean(bean);
+                        if (bean == null) return;
+                    }
+                }
+                if (condition.assertBean(bean)) {
+                    result.add(bean);
+                }
             }
 
             @Override
             public void keep(String line) throws Exception {
             }
-        }, new ConditionFilter(condition, beanClass));
+        }, new ConditionFilter(Condition.True, beanClass));
+
+        if (transactionLock.isLocked()) {
+            traverseAddAction:
+            for (int i = 0; i < transaction.size(); i++) {
+                Action action = transaction.get(i);
+                if (AddAction.class.equals(action.getClass())) {
+                    Bean bean = ((AddAction) action).element;
+                    for (int j = i + 1; j < transaction.size(); j++) {
+                        bean = transaction.get(j).modifyBean(bean);
+                        if (bean == null) {
+                            continue traverseAddAction;
+                        }
+                    }
+                    if (condition.assertBean(bean)) {
+                        result.add(bean);
+                    }
+                }
+            }
+        }
+
+        readLock.unlock();
         return result;
     }
 
@@ -137,9 +300,5 @@ final class FileBasedDataAccessComponent<Bean> implements DataAccessInterface<Be
         public boolean shouldBeFiltered(String line) {
             return condition.assertBean(JSON.parseObject(line, beanClass));
         }
-    }
-
-    private File getDataFile() {
-        return transaction ? tempFile : dataFile;
     }
 }
